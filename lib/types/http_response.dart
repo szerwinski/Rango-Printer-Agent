c4c2ccd6d22f27d3b6d2d5b1090ab7ba701a9032
lucasmma <lukito020@gmail.com>
2025-03-02 12:48:59 -0300

    feat: lint file and add fromMobile on Datum

--- lib/types/http_response.dart
+++ lib/types/http_response.dart
@@ -4,46 +4,54 @@
 
 import 'dart:convert';
 
-List<Table> tableFromJson(String str) => List<Table>.from(json.decode(str).map((x) => Table.fromJson(x)));
+List<Table> tableFromJson(String str) =>
+    List<Table>.from(json.decode(str).map((x) => Table.fromJson(x)));
 
-String tableToJson(List<Table> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String tableToJson(List<Table> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Table {
-    num? id;
-    String? name;
-    String? status;
-    bool? isTable;
-    DateTime? createdAt;
-    DateTime? updatedAt;
-    dynamic publishedAt;
-    num? index;
-    TableSale? tableSale;
+  num? id;
+  String? name;
+  String? status;
+  bool? isTable;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  dynamic publishedAt;
+  num? index;
+  TableSale? tableSale;
 
-    Table({
-        this.id,
-        this.name,
-        this.status,
-        this.isTable,
-        this.createdAt,
-        this.updatedAt,
-        this.publishedAt,
-        this.index,
-        this.tableSale,
-    });
+  Table({
+    this.id,
+    this.name,
+    this.status,
+    this.isTable,
+    this.createdAt,
+    this.updatedAt,
+    this.publishedAt,
+    this.index,
+    this.tableSale,
+  });
 
-    factory Table.fromJson(Map<String, dynamic> json) => Table(
+  factory Table.fromJson(Map<String, dynamic> json) => Table(
         id: json["id"],
         name: json["name"],
         status: json["status"],
         isTable: json["isTable"],
-        createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
-        updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
+        createdAt: json["createdAt"] == null
+            ? null
+            : DateTime.parse(json["createdAt"]),
+        updatedAt: json["updatedAt"] == null
+            ? null
+            : DateTime.parse(json["updatedAt"]),
         publishedAt: json["publishedAt"],
         index: json["index"],
-        tableSale: json["tableSale"] == null ? null : TableSale.fromJson(json["tableSale"]),
-    );
+        tableSale: json["tableSale"] == null
+            ? null
+            : TableSale.fromJson(json["tableSale"]),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "status": status,
@@ -53,48 +61,54 @@ class Table {
         "publishedAt": publishedAt,
         "index": index,
         "tableSale": tableSale?.toJson(),
-    };
+      };
 }
 
 class TableSale {
-    num? id;
-    String? status;
-    DateTime? createdAt;
-    DateTime? updatedAt;
-    dynamic authorizedSefaz;
-    dynamic xml;
-    dynamic xmlOffline;
-    dynamic publishedAt;
-    String? history;
-    List<Datum>? data;
+  num? id;
+  String? status;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  dynamic authorizedSefaz;
+  dynamic xml;
+  dynamic xmlOffline;
+  dynamic publishedAt;
+  String? history;
+  List<Datum>? data;
 
-    TableSale({
-        this.id,
-        this.status,
-        this.createdAt,
-        this.updatedAt,
-        this.authorizedSefaz,
-        this.xml,
-        this.xmlOffline,
-        this.publishedAt,
-        this.history,
-        this.data,
-    });
+  TableSale({
+    this.id,
+    this.status,
+    this.createdAt,
+    this.updatedAt,
+    this.authorizedSefaz,
+    this.xml,
+    this.xmlOffline,
+    this.publishedAt,
+    this.history,
+    this.data,
+  });
 
-    factory TableSale.fromJson(Map<String, dynamic> json) => TableSale(
+  factory TableSale.fromJson(Map<String, dynamic> json) => TableSale(
         id: json["id"],
         status: json["status"],
-        createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
-        updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
+        createdAt: json["createdAt"] == null
+            ? null
+            : DateTime.parse(json["createdAt"]),
+        updatedAt: json["updatedAt"] == null
+            ? null
+            : DateTime.parse(json["updatedAt"]),
         authorizedSefaz: json["authorizedSEFAZ"],
         xml: json["xml"],
         xmlOffline: json["xmlOffline"],
         publishedAt: json["publishedAt"],
         history: json["history"],
-        data: json["data"] == null ? [] : List<Datum>.from(json["data"]!.map((x) => Datum.fromJson(x))),
-    );
+        data: json["data"] == null
+            ? []
+            : List<Datum>.from(json["data"]!.map((x) => Datum.fromJson(x))),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "status": status,
         "createdAt": createdAt?.toIso8601String(),
@@ -104,49 +118,58 @@ class TableSale {
         "xmlOffline": xmlOffline,
         "publishedAt": publishedAt,
         "history": history,
-        "data": data == null ? [] : List<dynamic>.from(data!.map((x) => x.toJson())),
-    };
+        "data": data == null
+            ? []
+            : List<dynamic>.from(data!.map((x) => x.toJson())),
+      };
 }
 
 class Datum {
-    num? id;
-    num? quantity;
-    bool? fidelityRescue;
-    dynamic note;
-    String? status;
-    num? freePrice;
-    dynamic discount;
-    List<Option>? options;
-    MenuItem? menuItem;
-    String? uuid;
+  num? id;
+  num? quantity;
+  bool? fidelityRescue;
+  bool fromMobile;
+  dynamic note;
+  String? status;
+  num? freePrice;
+  dynamic discount;
+  List<Option>? options;
+  MenuItem? menuItem;
+  String? uuid;
 
-    Datum({
-        this.id,
-        this.quantity,
-        this.fidelityRescue,
-        this.note,
-        this.status,
-        this.freePrice,
-        this.discount,
-        this.options,
-        this.menuItem,
-        this.uuid
-    });
+  Datum(
+      {this.id,
+      this.quantity,
+      this.fidelityRescue,
+      this.note,
+      this.status,
+      this.freePrice,
+      this.discount,
+      this.options,
+      this.menuItem,
+      this.fromMobile = false,
+      this.uuid});
 
-    factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
         id: json["id"],
         quantity: json["quantity"],
         fidelityRescue: json["fidelityRescue"],
+        fromMobile: json["fromMobile"],
         note: json["note"],
         status: json["status"],
         freePrice: json["freePrice"],
         discount: json["discount"],
-        options: json["options"] == null ? [] : List<Option>.from(json["options"]!.map((x) => Option.fromJson(x))),
-        menuItem: json["menu_item"] == null ? null : MenuItem.fromJson(json["menu_item"]),
+        options: json["options"] == null
+            ? []
+            : List<Option>.from(
+                json["options"]!.map((x) => Option.fromJson(x))),
+        menuItem: json["menu_item"] == null
+            ? null
+            : MenuItem.fromJson(json["menu_item"]),
         uuid: json["uuid"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "quantity": quantity,
         "fidelityRescue": fidelityRescue,
@@ -154,72 +177,74 @@ class Datum {
         "status": status,
         "freePrice": freePrice,
         "discount": discount,
-        "options": options == null ? [] : List<dynamic>.from(options!.map((x) => x.toJson())),
+        "options": options == null
+            ? []
+            : List<dynamic>.from(options!.map((x) => x.toJson())),
         "menu_item": menuItem?.toJson(),
         "uuid": uuid
-    };
+      };
 }
 
 class MenuItem {
-    num? id;
-    String? name;
-    String? description;
-    String? largeImageUrl;
-    String? thumbnailImageUrl;
-    num? price;
-    bool? status;
-    bool? onSale;
-    num? promotionalPrice;
-    bool? generic;
-    DateTime? createdAt;
-    DateTime? updatedAt;
-    num? index;
-    String? icon;
-    String? promotionalType;
-    dynamic isScheduled;
-    num? code;
-    num? stock;
-    num? costPrice;
-    String? unit;
-    bool? byWeight;
-    String? ncm;
-    bool? blockPrinting;
-    String? barCode;
-    dynamic freePrice;
-    List<OptionsCategory>? optionsCategories;
-    MenuItemsCategory? menuItemsCategory;
+  num? id;
+  String? name;
+  String? description;
+  String? largeImageUrl;
+  String? thumbnailImageUrl;
+  num? price;
+  bool? status;
+  bool? onSale;
+  num? promotionalPrice;
+  bool? generic;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  num? index;
+  String? icon;
+  String? promotionalType;
+  dynamic isScheduled;
+  num? code;
+  num? stock;
+  num? costPrice;
+  String? unit;
+  bool? byWeight;
+  String? ncm;
+  bool? blockPrinting;
+  String? barCode;
+  dynamic freePrice;
+  List<OptionsCategory>? optionsCategories;
+  MenuItemsCategory? menuItemsCategory;
 
-    MenuItem({
-        this.id,
-        this.name,
-        this.description,
-        this.largeImageUrl,
-        this.thumbnailImageUrl,
-        this.price,
-        this.status,
-        this.onSale,
-        this.promotionalPrice,
-        this.generic,
-        this.createdAt,
-        this.updatedAt,
-        this.index,
-        this.icon,
-        this.promotionalType,
-        this.isScheduled,
-        this.code,
-        this.stock,
-        this.costPrice,
-        this.unit,
-        this.byWeight,
-        this.ncm,
-        this.blockPrinting,
-        this.barCode,
-        this.freePrice,
-        this.optionsCategories,
-        this.menuItemsCategory,
-    });
+  MenuItem({
+    this.id,
+    this.name,
+    this.description,
+    this.largeImageUrl,
+    this.thumbnailImageUrl,
+    this.price,
+    this.status,
+    this.onSale,
+    this.promotionalPrice,
+    this.generic,
+    this.createdAt,
+    this.updatedAt,
+    this.index,
+    this.icon,
+    this.promotionalType,
+    this.isScheduled,
+    this.code,
+    this.stock,
+    this.costPrice,
+    this.unit,
+    this.byWeight,
+    this.ncm,
+    this.blockPrinting,
+    this.barCode,
+    this.freePrice,
+    this.optionsCategories,
+    this.menuItemsCategory,
+  });
 
-    factory MenuItem.fromJson(Map<String, dynamic> json) => MenuItem(
+  factory MenuItem.fromJson(Map<String, dynamic> json) => MenuItem(
         id: json["id"],
         name: json["name"],
         description: json["description"],
@@ -230,8 +255,12 @@ class MenuItem {
         onSale: json["onSale"],
         promotionalPrice: json["promotionalPrice"]?.toDouble(),
         generic: json["generic"],
-        createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
-        updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
+        createdAt: json["createdAt"] == null
+            ? null
+            : DateTime.parse(json["createdAt"]),
+        updatedAt: json["updatedAt"] == null
+            ? null
+            : DateTime.parse(json["updatedAt"]),
         index: json["index"],
         icon: json["icon"],
         promotionalType: json["promotionalType"],
@@ -245,11 +274,16 @@ class MenuItem {
         blockPrinting: json["blockPrinting"],
         barCode: json["barCode"],
         freePrice: json["freePrice"],
-        optionsCategories: json["optionsCategories"] == null ? [] : List<OptionsCategory>.from(json["optionsCategories"]!.map((x) => OptionsCategory.fromJson(x))),
-        menuItemsCategory: json["menu_items_category"] == null ? null : MenuItemsCategory.fromJson(json["menu_items_category"]),
-    );
+        optionsCategories: json["optionsCategories"] == null
+            ? []
+            : List<OptionsCategory>.from(json["optionsCategories"]!
+                .map((x) => OptionsCategory.fromJson(x))),
+        menuItemsCategory: json["menu_items_category"] == null
+            ? null
+            : MenuItemsCategory.fromJson(json["menu_items_category"]),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "description": description,
@@ -275,44 +309,51 @@ class MenuItem {
         "blockPrinting": blockPrinting,
         "barCode": barCode,
         "freePrice": freePrice,
-        "optionsCategories": optionsCategories == null ? [] : List<dynamic>.from(optionsCategories!.map((x) => x.toJson())),
+        "optionsCategories": optionsCategories == null
+            ? []
+            : List<dynamic>.from(optionsCategories!.map((x) => x.toJson())),
         "menu_items_category": menuItemsCategory?.toJson(),
-    };
+      };
 }
 
 class MenuItemsCategory {
-    num? id;
-    String? name;
-    num? index;
-    DateTime? createdAt;
-    DateTime? updatedAt;
-    bool? rewardEnabled;
-    num? rewardPrize;
-    String? largeImageUrl;
+  num? id;
+  String? name;
+  num? index;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  bool? rewardEnabled;
+  num? rewardPrize;
+  String? largeImageUrl;
 
-    MenuItemsCategory({
-        this.id,
-        this.name,
-        this.index,
-        this.createdAt,
-        this.updatedAt,
-        this.rewardEnabled,
-        this.rewardPrize,
-        this.largeImageUrl,
-    });
+  MenuItemsCategory({
+    this.id,
+    this.name,
+    this.index,
+    this.createdAt,
+    this.updatedAt,
+    this.rewardEnabled,
+    this.rewardPrize,
+    this.largeImageUrl,
+  });
 
-    factory MenuItemsCategory.fromJson(Map<String, dynamic> json) => MenuItemsCategory(
+  factory MenuItemsCategory.fromJson(Map<String, dynamic> json) =>
+      MenuItemsCategory(
         id: json["id"],
         name: json["name"],
         index: json["index"],
-        createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
-        updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
+        createdAt: json["createdAt"] == null
+            ? null
+            : DateTime.parse(json["createdAt"]),
+        updatedAt: json["updatedAt"] == null
+            ? null
+            : DateTime.parse(json["updatedAt"]),
         rewardEnabled: json["rewardEnabled"],
         rewardPrize: json["rewardPrize"],
         largeImageUrl: json["largeImageUrl"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "index": index,
@@ -321,71 +362,77 @@ class MenuItemsCategory {
         "rewardEnabled": rewardEnabled,
         "rewardPrize": rewardPrize,
         "largeImageUrl": largeImageUrl,
-    };
+      };
 }
 
 class OptionsCategory {
-    num? id;
-    String? categoryName;
-    num? index;
-    bool? isOptional;
-    num? minOptions;
-    num? maxOptions;
-    List<Option>? options;
+  num? id;
+  String? categoryName;
+  num? index;
+  bool? isOptional;
+  num? minOptions;
+  num? maxOptions;
+  List<Option>? options;
 
-    OptionsCategory({
-        this.id,
-        this.categoryName,
-        this.index,
-        this.isOptional,
-        this.minOptions,
-        this.maxOptions,
-        this.options,
-    });
+  OptionsCategory({
+    this.id,
+    this.categoryName,
+    this.index,
+    this.isOptional,
+    this.minOptions,
+    this.maxOptions,
+    this.options,
+  });
 
-    factory OptionsCategory.fromJson(Map<String, dynamic> json) => OptionsCategory(
+  factory OptionsCategory.fromJson(Map<String, dynamic> json) =>
+      OptionsCategory(
         id: json["id"],
         categoryName: json["categoryName"],
         index: json["index"],
         isOptional: json["isOptional"],
         minOptions: json["minOptions"],
         maxOptions: json["maxOptions"],
-        options: json["options"] == null ? [] : List<Option>.from(json["options"]!.map((x) => Option.fromJson(x))),
-    );
+        options: json["options"] == null
+            ? []
+            : List<Option>.from(
+                json["options"]!.map((x) => Option.fromJson(x))),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "categoryName": categoryName,
         "index": index,
         "isOptional": isOptional,
         "minOptions": minOptions,
         "maxOptions": maxOptions,
-        "options": options == null ? [] : List<dynamic>.from(options!.map((x) => x.toJson())),
-    };
+        "options": options == null
+            ? []
+            : List<dynamic>.from(options!.map((x) => x.toJson())),
+      };
 }
 
 class Option {
-    num? id;
-    String? largeImageUrl;
-    String? thumbnailImageUrl;
-    String? name;
-    num? price;
-    dynamic description;
-    num? quantity;
-    bool? status;
+  num? id;
+  String? largeImageUrl;
+  String? thumbnailImageUrl;
+  String? name;
+  num? price;
+  dynamic description;
+  num? quantity;
+  bool? status;
 
-    Option({
-        this.id,
-        this.largeImageUrl,
-        this.thumbnailImageUrl,
-        this.name,
-        this.price,
-        this.description,
-        this.quantity,
-        this.status,
-    });
+  Option({
+    this.id,
+    this.largeImageUrl,
+    this.thumbnailImageUrl,
+    this.name,
+    this.price,
+    this.description,
+    this.quantity,
+    this.status,
+  });
 
-    factory Option.fromJson(Map<String, dynamic> json) => Option(
+  factory Option.fromJson(Map<String, dynamic> json) => Option(
         id: json["id"],
         largeImageUrl: json["largeImageUrl"],
         thumbnailImageUrl: json["thumbnailImageUrl"],
@@ -394,9 +441,9 @@ class Option {
         description: json["description"],
         quantity: json["quantity"],
         status: json["status"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "largeImageUrl": largeImageUrl,
         "thumbnailImageUrl": thumbnailImageUrl,
@@ -405,5 +452,5 @@ class Option {
         "description": description,
         "quantity": quantity,
         "status": status,
-    };
+      };
 }
